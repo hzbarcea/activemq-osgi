239d4a10ecff41721b17b5bab30cd17f1831965d
Hadrian Zbarcea <hadrian@apache.org>
2015-02-04 22:25:11 -0500

    Add custom handler sample

---
+++ plugins/custom-handler/src/main/java/org/example/activemq/custom/handler/CustomNamespaceHandler.java
@@ -0,0 +1,85 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.example.activemq.custom.handler;
+
+import java.net.URL;
+import java.util.Set;
+
+import org.apache.aries.blueprint.ParserContext;
+import org.osgi.service.blueprint.container.ComponentDefinitionException;
+import org.osgi.service.blueprint.reflect.BeanMetadata;
+import org.osgi.service.blueprint.reflect.ComponentMetadata;
+import org.osgi.service.blueprint.reflect.Metadata;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+
+public class CustomNamespaceHandler implements org.apache.aries.blueprint.NamespaceHandler {
+	// Namespaces supported
+    public static final String CUSTOM_NAMESPACE_V1_0 = "http://activemq.example.org/custom/handler/v1.0";
+
+    // Attributes and Elements supported
+    public static final String BEAN = "bean";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomNamespaceHandler.class);
+
+	@Override
+	public ComponentMetadata decorate(Node node, ComponentMetadata component, ParserContext context) {
+        LOGGER.debug("Decorate node {{}}{}", node.getNamespaceURI(), node.getLocalName());
+
+        if (node instanceof Element && nodeNameEquals(node, BEAN)) {
+            return context.parseElement(BeanMetadata.class, component, (Element)node);
+        } else {
+            throw new ComponentDefinitionException("Unsupported node: " + node.getNodeName());
+        }
+	}
+
+	@Override
+    @SuppressWarnings("rawtypes")
+	public Set<Class> getManagedClasses() {
+		// no compatibility checks
+		return null;
+	}
+
+	@Override
+	public URL getSchemaLocation(String namespace) {
+        if (CUSTOM_NAMESPACE_V1_0.equals(namespace)) {
+            return getClass().getResource("custom-handler-1.0.xsd");
+          }
+          return null;
+	}
+
+	@Override
+	public Metadata parse(Element element, ParserContext context) {
+        LOGGER.debug("Parsing element {{}}{}", element.getNamespaceURI(), element.getLocalName());
+
+        if (nodeNameEquals(element, BEAN)) {
+            return context.parseElement(BeanMetadata.class, context.getEnclosingComponent(), element);
+        } else {
+            throw new ComponentDefinitionException("Unsupported element: " + element.getNodeName());
+        }
+	}
+
+    private static boolean nodeNameEquals(Node node, String name) {
+        return (name.equals(node.getNodeName()) || name.equals(node.getLocalName()));
+    }
+
+}
+
